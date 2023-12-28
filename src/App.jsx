503d1458fae589feae1eba57c3ca128f869fb28e
Rumanito12 <ady_9697@yahoo.com>
2023-12-28 21:36:09 +0100

    ADD: se instala y se añade SASS y Bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Body } from "./pages/Body/Body";
 import { Header } from "./common/Header/Header";
 import { Footer } from "./common/Footer/Footer";
+import './App.scss'
 
 function App() {
   return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
+import Button from 'react-bootstrap/Button';
+import './Home.scss'
+
 export const Home = () => {
   return (
     <>
       <div>
         <h1>Home</h1>
+        <Button variant="custom-colors">Warning</Button>{' '}
       </div>
     </>
   );
