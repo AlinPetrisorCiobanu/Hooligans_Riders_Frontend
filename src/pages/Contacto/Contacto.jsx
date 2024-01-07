de9cc425fa5ce8d50ef458e2539435d2cfb5376e
Rumanito12 <ady_9697@yahoo.com>
2024-01-07 09:43:12 +0100

    ADD: se añade vista admin de todos los mensajes con paginación

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { userDate } from "../userSlice";
-import { newMessage } from "../../service/apiCalls";
+import { Custom_Pagination } from "../../common/Pagination/Pagination";
+import { getAllMessage, newMessage } from "../../service/apiCalls";
 
 export const Contacto = () => {
   //declaro constantes
@@ -16,7 +17,10 @@ export const Contacto = () => {
     data: "",
     message: "",
   });
-
+  const user = useSelector(userDate).user;
+  const [messages, setMessages] = useState([]);
+  const [pages, setPages] = useState("");
+  const [curent_page, setCurent_Page] = useState(1);
 
   //si no tienes token te manda a la pagina de inicio
   const tokenExist = (tokenEx) => {
@@ -46,8 +50,37 @@ export const Contacto = () => {
       });
   };
 
+  //sacar todos los mensajes
+
+  useEffect(() => {
+    getAllMessage(token, curent_page)
+      .then((res) => {
+        setMessages(res.data);
+        setPages(res.last_page);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [curent_page]);
+
+  const pagination = (data) => {
+    const cont = curent_page;
+    if (data === "first_page") {
+      setCurent_Page(1);
+    } else if (data === "prev") {
+      setCurent_Page(cont - 1);
+    } else if (data === "next") {
+      setCurent_Page(cont + 1);
+    } else if (data === "last_page") {
+      setCurent_Page(cont + 1);
+    } else {
+      setCurent_Page(data);
+    }
+  };
+
   return (
     <>
+      {user.role === "user" || user.role === "rider" ? (
         <div>
           <div>
             <label htmlFor="name">Nombre</label>
@@ -73,6 +106,21 @@ export const Contacto = () => {
             />
           </div>
         </div>
+      ) : (
+        <div>
+          {messages.map((mess)=>{
+            return(
+              <div key={(mess.id)}>
+                <h1>{mess.name}</h1>
+                <h3>{mess.last_name}</h3>
+                <h3>{mess.data}</h3>
+                <h3>{mess.message}</h3>
+                <hr />
+              </div>
+            )
+          })}
+        </div>
+      )}
     </>
   );
 };
